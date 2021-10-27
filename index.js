d984c260266ad2874aaa39ccb3bc96415b4476a8
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-27 07:35:35 -0500

    Update to include server build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateGitignore = require("./utils/generateGitignore");
+const generateServer = require("./utils/generateServer");
 
 const setupQuestions = [
   {
@@ -19,7 +20,7 @@ const setupQuestions = [
   },
 ];
 
-// fn to initialize question prompts on run of node index
+// fn to initialize question prompts, create project on run of node index
 function init() {
   return inquirer.prompt(setupQuestions).then((response) => {
     const dir = "./dist";
@@ -46,6 +47,41 @@ function init() {
     // add .gitignore to server directory
     fs.writeFile(`./dist/${response.title}/server/.gitignore`, generateGitignore(), (err) => {
       console.log("A .gitignore has been added to the server directory. Continuing server build...")
+      if (err) throw err
+  })
+    // create config directory
+    const configDir = `./dist/${response.title}/server/config`;
+    fs.mkdirSync(configDir);
+    console.log(
+      "A config directory has been added to the server directory. The build continues..."
+    ); 
+    // create models directory
+    const modelsDir = `./dist/${response.title}/server/models`;
+    fs.mkdirSync(modelsDir);
+    console.log(
+      "A models directory has been added to the server directory. The build marches on..."
+    ); 
+    // create schemas directory
+    const schemasDir = `./dist/${response.title}/server/schemas`;
+    fs.mkdirSync(schemasDir);
+    console.log(
+      "We've got a schemas directory in the server directory! The grind continues..."
+    ); 
+    // create seeders directory
+    const seedersDir = `./dist/${response.title}/server/seeders`;
+    fs.mkdirSync(seedersDir);
+    console.log(
+      "Your server's seeds now have a home in the seeders directory! Can't wait to see what you grow..."
+    ); 
+    // create util directory
+    const utilDir = `./dist/${response.title}/server/util`;
+    fs.mkdirSync(utilDir);
+    console.log(
+      "Take this util directory. It's dangerous to go alone..."
+    );
+    // create server.js in server directory
+    fs.writeFile(`./dist/${response.title}/server/server.js`, generateServer(), (err) => {
+      console.log("We've got a server! Now let's establish a connection to our database...")
       if (err) throw err
   }) 
   });
