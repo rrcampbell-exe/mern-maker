a4868a5be1eea2653e4e6134b9808015ad5349b8
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-27 07:52:09 -0500

    Create connection.js file

---
+++ utils/generateConnection.js
@@ -0,0 +1,21 @@
+
+const generateConnection = (response) => {
+  // js to format db name based on project name
+  dbName = response.title.toLowerCase()
+  dbName = dbName.replace(/\s+/g, '-');
+
+  return `
+  const mongoose = require('mongoose');
+
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/${dbName}', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+  
+  module.exports = mongoose.connection; 
+  `
+}
+
+module.exports = generateConnection
