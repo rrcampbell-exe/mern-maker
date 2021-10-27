ec3cfd5e0204adf68ae0ad61c7952fb4ef381b8c
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-27 07:14:19 -0500

    Create server directory, add .gitignore to it and root

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require access to file system, import inquirer package
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateGitignore = require("./utils/generateGitignore");
 
 const setupQuestions = [
   {
@@ -25,11 +26,28 @@ function init() {
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
+    // create root directory
     const rootDir = `./dist/${response.title}`;
     fs.mkdirSync(rootDir);
     console.log(
       "The root directory for " + response.title + " has been generated! Creating files..."
     );
+    // add .gitignore to root directory
+    fs.writeFile(`./dist/${response.title}/.gitignore`, generateGitignore(), (err) => {
+      console.log("A .gitignore has been added to the root directory. Onto the server...")
+      if (err) throw err
+  }) 
+    // create server directory
+    const serverDir = `./dist/${response.title}/server`;
+    fs.mkdirSync(serverDir);
+    console.log(
+      "The server directory for " + response.title + " has been generated! Building server..."
+    );
+    // add .gitignore to server directory
+    fs.writeFile(`./dist/${response.title}/server/.gitignore`, generateGitignore(), (err) => {
+      console.log("A .gitignore has been added to the server directory. Continuing server build...")
+      if (err) throw err
+  }) 
   });
 }
 
