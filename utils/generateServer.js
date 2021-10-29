3b321c311b4746c0a3b35007be3129a4a7def66a
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-29 06:45:34 -0500

    Comment out code that prevents run of Apollo Client while in development

--- utils/generateServer.js
+++ utils/generateServer.js
@@ -38,9 +38,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// remove comment from below at time of deployment!
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // Initialize server
 startServer();
